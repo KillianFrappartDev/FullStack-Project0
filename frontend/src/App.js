c7c07d8169f0fa4ae42d0a9d29319d991a2293d4
Killian Frappart <Killianfrappartdev@gmail.com>
2020-07-27 14:12:56 +0200

    Adding core components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,25 @@ import {
   Switch,
 } from "react-router-dom";
 
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/places/new' exact>
-          <NewPlace />
-        </Route>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/places/new' exact>
+            <NewPlace />
+          </Route>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
   );
 }
--- frontend/src/user/components/UserList.js
+++ frontend/src/user/components/UserList.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserList.css";
 
 const UserList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className='center'>
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   } else {
     return (
-      <ul className="users-list">
+      <ul className='users-list'>
         {props.items.map((user) => (
           <UserItem
             key={user.id}
