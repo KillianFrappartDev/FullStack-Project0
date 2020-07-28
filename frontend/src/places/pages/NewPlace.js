16b276b4ffd36519a02a8087d68ae49805a7dfca
Killian Frappart <Killianfrappartdev@gmail.com>
2020-07-28 18:15:05 +0200

    Form Validation and Frontend components

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './NewPlace.css';
+
 const NewPlace = () => {
-    return <h2>New Place works!</h2>;
+    return (
+        <form className="place-form">
+            <Input element='input' type='text' label='Title' errorText="Please enter a valid title." validators={[VALIDATOR_REQUIRE()]} />
+        </form>
+    );
 }
 
 export default NewPlace;
